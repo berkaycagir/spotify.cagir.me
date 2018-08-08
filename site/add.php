d44fe304733eb82f1b8fae782c20638183d510f1
berkaycagir <berkay@cagir.me>
2018-08-08 15:05:48 +0300

    accepting song or album link as input

--- site/add.php
+++ site/add.php
@@ -3,13 +3,13 @@ include "header.php";
 include "config.php";
 
 if(empty($_POST['id']) || empty($_POST['country']) || empty($_POST['mail'])) {
-	echo "One or more required parameters have not been sent.";
-	include "footer.php";
+    echo "One or more required parameters have not been sent.";
+    include "footer.php";
     exit();
 }
 
 // setting variables, provided all three available
-$id = $_POST['id'];
+$id = sanitize_id($_POST['id'], get_access_token($spotify_auth_token, $spotify_refresh_token));
 $country = $_POST['country'];
 $mail = $_POST['mail'];
 
@@ -39,48 +39,83 @@ function get_access_token($spotify_auth_token, $spotify_refresh_token) {
 }
 
 function get_song($id, $spotify_headers) {
-	$url = "https://api.spotify.com/v1/tracks/".$id;
+    $url = "https://api.spotify.com/v1/tracks/".$id;
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $spotify_headers);
-	$result = curl_exec($ch);
-	curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $spotify_headers);
+    $result = curl_exec($ch);
+    curl_close($ch);
 
-	return json_decode($result, true);
+    return json_decode($result, true);
+}
+
+function get_first_song_of_album($id, $spotify_headers) {
+        $url = "https://api.spotify.com/v1/albums/".$id."/tracks?offset=0&limit=1";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $spotify_headers);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result, true)["items"][0]["id"];
 }
 
 function get_countries() {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL, 'http://country.io/names.json');
-	$result = curl_exec($ch);
-	curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, 'http://country.io/names.json');
+    $result = curl_exec($ch);
+    curl_close($ch);
 
-	return json_decode($result, true);
+    return json_decode($result, true);
 }
 
 function is_track_relinked($id, $country, $spotify_headers) {
-	$url = "https://api.spotify.com/v1/tracks/".$id."?market=".$country;
+    $url = "https://api.spotify.com/v1/tracks/".$id."?market=".$country;
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $spotify_headers);
-	$result = curl_exec($ch);
-	curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $spotify_headers);
+    $result = curl_exec($ch);
+    curl_close($ch);
 
-	$obj = json_decode($result, true);
+    $obj = json_decode($result, true);
 
-	if($obj["is_playable"]) {
-		return $obj["uri"];
-	} else {
-		return false;
-	}
+    if($obj["is_playable"]) {
+        return $obj["uri"];
+    } else {
+        return false;
+    }
+}
+
+function sanitize_id($input, $spotify_headers) {
+        $retval = "";
+        if(preg_match("/https?:\/\/open.spotify.com\/(album|track)\/(.+?)(?=\?|\n|$)/", $input, $matches)) {
+                if($matches[1] == "track") {
+                        $retval = $matches[2];
+                } else {
+                        $retval = get_first_song_of_album($matches[2], $spotify_headers);
+                }
+        } else {
+                $retval = $input;
+        }
+
+        if(!preg_match("/^(\w*)$/", $retval)) {
+                echo "Song ID is not valid.";
+                include "footer.php";
+                exit();
+        }
+
+        return $retval;
 }
 
 // functions end
@@ -94,7 +129,6 @@ if(($linked_track_id = is_track_relinked($id, $country, get_access_token($spotif
 
 // getting song if existent
 $obj = get_song($id, get_access_token($spotify_auth_token, $spotify_refresh_token));
-
 // getting countries
 $countries = get_countries();
 
